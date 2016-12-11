0ffea86d903b5647d042d2c9c1ede2b3582fa058
Web Artist <nomi.sheikh@gmail.com>
2016-12-11 21:26:38 +0400

    Added functionality for posting comments.

--- tumblr/app/controllers/application_controller.rb
+++ tumblr/app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def create
+  	@post = Post.find(params[:post])
+  	@comment = @post.comments.create(params[:comment].permit(:name, :body))
+
+  	redirect_to post_path(@post)
+  end
 end
