4ddf11c3043cfde45f46597afc145a2c47287b86
Web Artist <nomi.sheikh@gmail.com>
2016-12-14 01:15:01 +0400

    Added devise and associations with Pics.

--- instagram/app/controllers/pics_controller.rb
+++ instagram/app/controllers/pics_controller.rb
@@ -1,5 +1,49 @@
 class PicsController < ApplicationController
-
+	before_action :find_pic, only: [:show, :edit, :update, :destroy]
 	def index
+		@pics = Pic.all.order("created_at DESC")
+	end
+
+	def show
+		find_pic
+	end
+	def new
+		@pic = current_user.pics.build
+	end
+
+	def create
+		@pic = current_user.pics.build(pic_params)
+
+		if @pic.save
+			redirect_to @pic, notice: "Yesss! It was posted."
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+
+	end
+
+	def update
+		if @pic.update(pic_params)
+			redirect_to @pic, notice: "Congrats! Pic is updated successfully."
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@pic.destroy
+		redirect_to root_path
+	end
+
+	private
+	def pic_params
+		params.require(:pic).permit(:title, :description)
+	end
+
+	def find_pic
+		@pic = Pic.find(params[:id])
 	end
 end
